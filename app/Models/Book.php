54181232636d48965c1d0a5d08bc9e5f40037462
susana <susanabeltran679@gmail.com>
2026-02-19 08:42:06 -0600

    fillable de libro

--- app/Models/Book.php
+++ app/Models/Book.php
@@ -2,9 +2,31 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Prompts\Concerns\Fallback;
 
 class Book extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'title',
+        'description',
+        'isbn',
+        'total_copies',
+        'available_copies',
+        'status',
+    ];
+
+    protected $casts = [
+        'status' => 'boolean',
+        'total_copies' => 'integer',
+        'available_copies' => 'integer',
+    ];
+
+    public function loans()
+    {
+        return $this->hasMany(Loan::class);
+    }
 }
