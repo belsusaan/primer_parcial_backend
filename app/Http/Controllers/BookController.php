4b9cdd40cbc3d68c54540c3f17ca7724ad2c2452
algodoncin <179851883+algodondev@users.noreply.github.com>
2026-02-19 08:53:35 -0600

    feat: set BookController

--- app/Http/Controllers/BookController.php
+++ app/Http/Controllers/BookController.php
@@ -6,5 +6,22 @@ use Illuminate\Http\Request;
 
 class BookController extends Controller
 {
-    //
+    public function index(Request $request)
+    {
+        $query = Book::query();
+
+        if ($request->has('title')) {
+            $query->where('title', 'like', '%' . $request->title . '%');
+        }
+
+        if ($request->has('isbn')) {
+            $query->where('isbn', $request->isbn);
+        }
+
+        if ($request->has('status')) {
+            $query->where('status', filter_var($request->status, FILTER_VALIDATE_BOOLEAN));
+        }
+
+        return BookResource::collection($query->get());
+    }
 }
